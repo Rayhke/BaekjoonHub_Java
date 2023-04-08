545e0b1acb2062156555f8c7b9a66b660cae5f27
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-08 17:22:35 +0900

    Update Hello World.java

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -3,3 +3,9 @@ public class Main {
         System.out.print("Hello World!");
     }
 }
+/* ■ 설명 ■
+[System.out.print] 이랑 [System.out.println] 는
+c언어에 [printf("\n");] 에 들어가는 [\n] 와 기능이 같다.
+즉, 작동 후 줄바꿈이 되도록 하고자 하면 반드시
+[println] 를 사용할 것
+*/
