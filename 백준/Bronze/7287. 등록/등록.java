66ff07c6476973e8260f1b4c7e5c231d070d99de
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-22 18:00:06 +0900

    Update 등록.java

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -1,5 +1,5 @@
-public class Main {
-	public static void main(String[] args) {		
-		System.out.print("134\nnink2458");
+public class Main {					// 반드시 이 코드 형태를 유지할 것
+	public static void main(String[] args) {
+		System.out.print("134\nnink2458");	// 백준에서 문제 풀은 갯수와 아이디 출력
 	}
 }
