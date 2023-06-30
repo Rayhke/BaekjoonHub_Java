2e381a85fe7e1b3546339efe1ef6a02773e8c9e7
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-30 14:02:41 +0900

    Update A－B.java

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -2,16 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
 	public static void main(String[] args) throws IOException {		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		String[] strs = br.readLine().split(" ");
-		
 		int a = Integer.parseInt(strs[0]);
 		int b = Integer.parseInt(strs[1]);
-		
 		System.out.print(a - b);
 	}
-	
 }
+/* ■ 설명 ■
+[a] 와 [b] 를 입력 받고, ' a - b ' 결과값을 출력하라.
+*/
