7c85776602441f1f1960f50d06959df9298a4fe7
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-06 17:48:37 +0900

    [Silver IV] Title: 큐, Time: 388 ms, Memory: 21300 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader p = new BufferedReader(new InputStreamReader(System.in));
+        int a, b, c = -1;
+        String s;
+        a = Integer.parseInt(p.readLine());
+        int[] d = new int[a + 1];
+        for (int n = 0; n < a; n++) {
+            String[] s2 = p.readLine().split(" ");
+            s = s2[0];
+            if (s.equals("push")) {
+                b = Integer.parseInt(s2[1]);
+                d[++c] = b;
+            } else if (s.equals("pop")) {
+                if (c != -1) {
+                    System.out.println(d[0]);
+                    for(int m = 0; m < c; m++) { d[m] = d[m + 1]; }
+                    c--;
+                } else { System.out.println(-1); }
+            } else if (s.equals("size")) {
+                if (c != -1) { System.out.println(c + 1); }
+                else { System.out.println(0); }
+            } else if (s.equals("empty")) {
+                if (c != -1) { System.out.println(0); }
+                else { System.out.println(1); }
+            } else if (s.equals("front")) {
+                if (c != -1) { System.out.println(d[0]); }
+                else { System.out.println(-1); }
+            } else if (s.equals("back")) {
+                if (c != -1) { System.out.println(d[c]); }
+                else { System.out.println(-1); }
+            }
+        }
+    }
+}
