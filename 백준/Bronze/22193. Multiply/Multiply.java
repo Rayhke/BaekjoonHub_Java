32c686c5feadefde161af40c7a2b36266113563f
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-04-15 11:55:07 +0900

    [Bronze V] Title: Multiply, Time: 712 ms, Memory: 55240 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]), M = Integer.parseInt(input[1]);
+        BigInteger A = new BigInteger(br.readLine()), B = new BigInteger(br.readLine());
+        System.out.print(A.multiply(B));
+    }
+}
